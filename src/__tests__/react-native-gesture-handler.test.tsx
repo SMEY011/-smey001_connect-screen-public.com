a45cbf8519257e8ca9b6f0b05ab025a7cb0059b1
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-22 10:02:19 +0100

    root view

--- src/__tests__/react-native-gesture-handler.test.tsx
+++ src/__tests__/react-native-gesture-handler.test.tsx
@@ -1,40 +1,52 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { Pressable } from 'react-native-gesture-handler';
+import { GestureHandlerRootView, Pressable } from 'react-native-gesture-handler';
 import { render, screen } from '../pure';
 
 test('GestureHandler Pressable', () => {
   const onPress = jest.fn();
 
   render(
-    <Pressable onPress={onPress}>
-      <Text>Hello</Text>
-    </Pressable>,
+    <GestureHandlerRootView>
+      <Pressable onPress={onPress}>
+        <Text>Hello</Text>
+      </Pressable>
+      ,
+    </GestureHandlerRootView>,
   );
 
   expect(screen.toJSON()).toMatchInlineSnapshot(`
-    <RNGestureHandlerButton
-      collapsable={false}
-      enabled={true}
-      hitSlop={
+    <View
+      style={
         {
-          "bottom": 0,
-          "left": 0,
-          "right": 0,
-          "top": 0,
+          "flex": 1,
         }
       }
-      rippleColor={0}
-      style={
-        [
-          {},
-          undefined,
-        ]
-      }
     >
-      <Text>
-        Hello
-      </Text>
-    </RNGestureHandlerButton>
+      <RNGestureHandlerButton
+        collapsable={false}
+        enabled={true}
+        hitSlop={
+          {
+            "bottom": 0,
+            "left": 0,
+            "right": 0,
+            "top": 0,
+          }
+        }
+        rippleColor={0}
+        style={
+          [
+            {},
+            undefined,
+          ]
+        }
+      >
+        <Text>
+          Hello
+        </Text>
+      </RNGestureHandlerButton>
+      ,
+    </View>
   `);
 });
